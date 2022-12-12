b7f1c68f622239b8cc5e68c11d6a3526fc9a5686
Hapulus <59167675+hapulus@users.noreply.github.com>
2022-12-12 14:18:37 +0800

    Update run.sh

--- lab_advanced/lab_SIMD/run.sh
+++ lab_advanced/lab_SIMD/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm *.out
 gcc -o test.out -msse -mavx test.c -w
-sudo perf record ./test.out -m 100 -n 1000 -r 100
+sudo perf record ./test.out -m 4096 -n 4096 -r 4096
 sudo perf report > perf.txt
 
 # perf probe -x ./test.out <func name>
