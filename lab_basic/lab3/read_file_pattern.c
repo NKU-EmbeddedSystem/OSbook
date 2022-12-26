48c6382d83503afce5a522a8207c521665e08a39
kjr829 <77659571+kjr829@users.noreply.github.com>
2022-12-26 20:18:41 +0800

    Update read_file_pattern.c

--- lab_basic/lab3/read_file_pattern.c
+++ lab_basic/lab3/read_file_pattern.c
@@ -10,8 +10,8 @@
 #include<regex.h>
 
 int main(){
-    //int fd=open("test.txt",O_RDONLY);
-    int fd=open("/home/kjr/OSbook/bible10.txt",O_RDONLY);
+    int fd=open("new.txt",O_RDONLY);
+    //int fd=open("/home/kjr/OSbook/bible10.txt",O_RDONLY);
     if(fd==-1){
         printf("can't open the file");
         return 1;
@@ -21,7 +21,7 @@ int main(){
     regmatch_t pmatch[1];
     regex_t reg;
     int count=0;
-    char pattern[]="[0-9]{5}";
+    char pattern[]="^From ilug-admin@linux.ie.*Aug.*";//查找ilug-admin@linux.ie在八月份发送的邮件
      
     status=regcomp(&reg,pattern,REG_EXTENDED|REG_NEWLINE);
     if(status!=0){
@@ -30,8 +30,8 @@ int main(){
     }
     char output[1024]={"\0"};
     while(len>0){
-        char buf[1024]={"\0"};
-        len=read(fd,buf,1024);
+        char buf[2048]={"\0"};
+        len=read(fd,buf,2048);
         char *temp=buf;
         while(1){
             status=regexec(&reg,temp,1,pmatch,0);
