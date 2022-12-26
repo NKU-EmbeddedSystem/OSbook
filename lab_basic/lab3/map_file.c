f2066815cd0416adecd1aef4cd183038e75133b6
kjr829 <77659571+kjr829@users.noreply.github.com>
2022-12-26 20:23:21 +0800

    Update map_file.c

--- lab_basic/lab3/map_file.c
+++ lab_basic/lab3/map_file.c
@@ -7,32 +7,7 @@
 #include<time.h>
 #include<sys/time.h>
 
-#define VMRSS_LINE 22
-
-unsigned int get_proc_mem(unsigned int pid){
-	char file_name[64]={0};
-	sprintf(file_name,"/proc/%d/status",pid);
-	
-    FILE *fd;
-	char line_buff[512]={0};
-	fd =fopen(file_name,"r");
-	if(fd==NULL) return 0;
-	char name[64];
-	int vmrss;
-	for (int i=0; i<VMRSS_LINE-1;i++){
-		fgets(line_buff,sizeof(line_buff),fd);
-	}
-	
-	fgets(line_buff,sizeof(line_buff),fd);
-	sscanf(line_buff,"%s %d",name,&vmrss);
-	fclose(fd);
- 
-	return vmrss;//程序使用的物理内存
-}
-
 int main(){
-    int pid=getpid();
-    printf("pid=%d\n",pid);
     struct timeval tv;
     long startTime=0;
     long endTime=0;
@@ -40,8 +15,8 @@ int main(){
     startTime=tv.tv_sec*1000000+tv.tv_usec;//tv_sec=s;tv_usec=us;
     printf("startTime:%ld\n",startTime);
 
-    //int fd=open("test.txt",O_RDONLY);
-    int fd=open("/home/kjr/OSbook/bible10.txt",O_RDONLY);
+    int fd=open("new.txt",O_RDONLY);
+    //int fd=open("/home/kjr/OSbook/bible10.txt",O_RDONLY);
     if(fd==-1){
         printf("can't open the file");
         return 1;
@@ -89,6 +64,5 @@ int main(){
     printf("endTime:%ld\n",endTime);
     printf("used time is %ld us\n",(endTime-startTime));
 
-    printf("used memory=%d kB\n",get_proc_mem(pid));
     return 0;
 }
