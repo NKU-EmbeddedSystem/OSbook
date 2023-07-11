1fa287fec0a21d9e1bb27047edaa5e36c52db1a7
HongShao0000 <52363750+HongShao0000@users.noreply.github.com>
2023-07-11 16:11:13 +0800

    Update blockmm.cpp

--- lab_advanced/lab_cache/blockmm.cpp
+++ lab_advanced/lab_cache/blockmm.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <chrono> // Ìí¼ÓĞÔÄÜ²âÊÔËùĞèµÄÍ·ÎÄ¼ş
-#include <cstdlib> // Ìí¼ÓC±ê×¼¿âÍ·ÎÄ¼ş
+#include <chrono> // æ·»åŠ æ€§èƒ½æµ‹è¯•æ‰€éœ€çš„å¤´æ–‡ä»¶
+#include <cstdlib> // æ·»åŠ Cæ ‡å‡†åº“å¤´æ–‡ä»¶
 
 using namespace std;
-using namespace std::chrono; // ¸ß¾«¶ÈÊ±¼ä¿âÃüÃû¿Õ¼ä
+using namespace std::chrono; // é«˜ç²¾åº¦æ—¶é—´åº“å‘½åç©ºé—´
+#define blockSize 128
 
-
-// Ö±½ÓÊµÏÖµÄ¾ØÕó³Ë·¨
-vector<vector<int>> matrixMultiplicationDirect(const vector<vector<int>>& A, const vector<vector<int>>& B) {
+// ç›´æ¥å®ç°çš„çŸ©é˜µä¹˜æ³•
+vector<vector<float>> matrixMultiplicationDirect(const vector<vector<float>>& A, const vector<vector<float>>& B) {
     int m = A.size();
     int n = A[0].size();
     int p = B[0].size();
 
-    vector<vector<int>> C(m, vector<int>(p, 0));
+    vector<vector<float>> C(m, vector<float>(p, 0.0));
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < p; j++) {
@@ -26,55 +26,12 @@ vector<vector<int>> matrixMultiplicationDirect(const vector<vector<int>>& A, con
     return C;
 }
 
-// Õë¶Ô»º´æÓÅ»¯µÄ¾ØÕó³Ë·¨
-vector<vector<int>> matrixMultiplicationCacheOptimized(const vector<vector<int>>& A, const vector<vector<int>>& B) {
+// åˆ†å—çŸ©é˜µä¹˜æ³•
+vector<vector<float>> matrixMultiplicationBlocked1(const vector<vector<float>>& A, const vector<vector<float>>& B) {
     int m = A.size();
     int n = A[0].size();
     int p = B[0].size();
-
-    vector<vector<int>> C(m, vector<int>(p, 0));
-
-    for (int i = 0; i < m; i++) {
-        for (int k = 0; k < n; k++) {
-            for (int j = 0; j < p; j++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    return C;
-}
-
-// ·Ö¿é¾ØÕó³Ë·¨
-vector<vector<int>> matrixMultiplicationBlockedCacheOptimized(const vector<vector<int>>& A, const vector<vector<int>>& B) {
-    int m = A.size();
-    int n = A[0].size();
-    int p = B[0].size();
-    int blockSize = 50;
-    vector<vector<int>> C(m, vector<int>(p, 0));
-
-    for (int kk = 0; kk < n; kk += blockSize) {
-        for (int jj = 0; jj < p; jj += blockSize) {
-            for (int i = 0; i < m; i++) {
-                for (int k = kk; k < min(kk + blockSize, n); k++) {
-                    for (int j = jj; j < min(jj + blockSize, p); j++) {
-                        C[i][j] += A[i][k] * B[k][j];
-                    }
-                }
-            }
-        }
-    }
-
-    return C;
-}
-
-// Õë¶ÔcacheÓÅ»¯µÄ·Ö¿é¾ØÕó³Ë·¨
-vector<vector<int>> matrixMultiplicationBlocked(const vector<vector<int>>& A, const vector<vector<int>>& B) {
-    int m = A.size();
-    int n = A[0].size();
-    int p = B[0].size();
-    int blockSize = 50;
-    vector<vector<int>> C(m, vector<int>(p, 0));
+    vector<vector<float>> C(m, vector<float>(p, 0.0));
 
     for (int kk = 0; kk < n; kk += blockSize) {
         for (int jj = 0; jj < p; jj += blockSize) {
@@ -91,8 +48,32 @@ vector<vector<int>> matrixMultiplicationBlocked(const vector<vector<int>>& A, co
     return C;
 }
 
-//½á¹ûÑéÖ¤º¯Êı
-bool verifyMatrixEquality(const vector<vector<int>>& A, const vector<vector<int>>& B) {
+// é’ˆå¯¹cacheä¼˜åŒ–çš„åˆ†å—çŸ©é˜µä¹˜æ³•
+vector<vector<float>> matrixMultiplicationBlocked2(const vector<vector<float>>& A, const vector<vector<float>>& B) {
+    int m = A.size();
+    int n = A[0].size();
+    int p = B[0].size();
+    vector<vector<float>> C(m, vector<float>(p, 0.0));
+
+    for (int kk = 0; kk < n; kk += blockSize) {
+        for (int jj = 0; jj < p; jj += blockSize) {
+            for (int i = 0; i < m; i++) {
+                for (int k = kk; k < min(kk + blockSize, n); k++) {
+                    for (int j = jj; j < min(jj + blockSize, p); j++) {
+                        C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    return C;
+}
+
+
+
+//ç»“æœéªŒè¯å‡½æ•°
+bool verifyMatrixEquality(const vector<vector<float>>& A, const vector<vector<float>>& B) {
     if (A.size() != B.size() || A[0].size() != B[0].size()) {
         return false;
     }
@@ -111,45 +92,44 @@ bool verifyMatrixEquality(const vector<vector<int>>& A, const vector<vector<int>
     return true;
 }
 
-// ĞÔÄÜ²âÊÔº¯Êı
-void performanceTest(const vector<vector<int>>& A, const vector<vector<int>>& B, const string& testName,
-                     vector<vector<int>> (*multiplyFunction)(const vector<vector<int>>&, const vector<vector<int>>&) ) {
-    auto start = high_resolution_clock::now(); // ¼ÇÂ¼¿ªÊ¼Ê±¼ä
+// æ€§èƒ½æµ‹è¯•å‡½æ•°
+void performanceTest(const vector<vector<float>>& A, const vector<vector<float>>& B, const string& testName,
+                     vector<vector<float>> (*multiplyFunction)(const vector<vector<float>>&, const vector<vector<float>>&) ) {
+    auto start = high_resolution_clock::now(); // è®°å½•å¼€å§‹æ—¶é—´
 
-    // Ö´ĞĞ¾ØÕó³Ë·¨
-    vector<vector<int>> result = multiplyFunction(A, B);
+    // æ‰§è¡ŒçŸ©é˜µä¹˜æ³•
+    vector<vector<float>> result = multiplyFunction(A, B);
 
-    auto end = high_resolution_clock::now(); // ¼ÇÂ¼½áÊøÊ±¼ä
-    auto duration = duration_cast<nanoseconds>(end - start); // ¼ÆËãÖ´ĞĞÊ±¼ä
+    auto end = high_resolution_clock::now(); // è®°å½•ç»“æŸæ—¶é—´
+    auto duration = duration_cast<nanoseconds>(end - start); // è®¡ç®—æ‰§è¡Œæ—¶é—´
 
-    double time = duration.count() / 1e9; // ×ª»»ÎªÃë
+    double time = duration.count() / 1e9; // è½¬æ¢ä¸ºç§’
     double size = A.size(); 
-    double gflops = (2.0 * size * size * size) / time / 1e9; // ¼ÆËãGFLOPS
+    double gflops = (2.0 * size * size * size) / time / 1e9; // è®¡ç®—GFLOPS
 
     cout << testName << "Time: " << duration.count() << " nanoseconds" << endl;
     cout << testName << " GFLOPS: " << gflops << endl;
 
-    vector<vector<int>> referenceResult = matrixMultiplicationDirect(A, B);
-    if (verifyMatrixEquality(referenceResult, result)) {
-        cout << "Result correct!" << endl;
-    } else {
-        cout << "Result wrong!" << endl;
-    }
+    // vector<vector<float>> referenceResult = matrixMultiplicationDirect(A, B);
+    // if (verifyMatrixEquality(referenceResult, result)) {
+    //     cout << "Result correct!" << endl;
+    // } else {
+    //     cout << "Result wrong!" << endl;
+    // }
 }
 
 
 int main() {
-    // ´´½¨Á½¸ö´ó¾ØÕó
-    int size = 1000; // ¾ØÕó´óĞ¡£¬¿ÉÒÔ¸ù¾İĞèÒªµ÷Õû
-    vector<vector<int>> A(size, vector<int>(size, 1));
-    vector<vector<int>> B(size, vector<int>(size, 2));
+    // åˆ›å»ºä¸¤ä¸ªå¤§çŸ©é˜µ
+    int size = 1024; // çŸ©é˜µå¤§å°ï¼Œå¯ä»¥æ ¹æ®éœ€è¦è°ƒæ•´
+    vector<vector<float>> A(size, vector<float>(size, 1.0));
+    vector<vector<float>> B(size, vector<float>(size, 2.0));
 
 
     double totalFlops = 2.0 * size * size * size;
-    performanceTest(A, B, "Before optimization:", matrixMultiplicationDirect);
-    //performanceTest(A, B, "After Changing Order optimization:", matrixMultiplicationCacheOptimized);
-    performanceTest(A, B, "Blocked & Cache Optimized Multiplication: ", matrixMultiplicationBlockedCacheOptimized);
-    performanceTest(A, B, "Blocked Multiplication: ", matrixMultiplicationBlocked);
 
+    performanceTest(A, B, "Blocked Multiplication: ", matrixMultiplicationDirect);
+    //performanceTest(A, B, "Blocked Multiplication: ", matrixMultiplicationBlocked1);
+    // performanceTest(A, B, "Blocked Multiplication: ", matrixMultiplicationBlocked2);
     
 }
