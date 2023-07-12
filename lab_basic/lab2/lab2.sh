e477ae799f83eb13ca26cfc0399e571e2b71c0cd
cltinga <91513458+cltinga@users.noreply.github.com>
2023-07-12 15:49:34 +0800

    Update lab2.sh

--- lab_basic/lab2/lab2.sh
+++ lab_basic/lab2/lab2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-for i in `ls`
+dir=$(dirname "$0")/input
+
+unzip -q "$dir/ham.zip" -d "$dir"  # 解压 ham.zip
+
+for i in "$dir/ham"/*
 do
    filename=$i
-#查找主题为回复的邮件并打印第一行内容，即发送者和日期
-   if [ `grep -c "^Subject: *R[eE]" $filename` -ne '0' ];then #即存在一行以包含Re或RE的Subject
-     awk 'NR==1' $filename >> new.txt
+   # 查找主题为回复的邮件并打印第一行内容，即发送者和日期
+   if [ $(grep -c "^Subject: *R[eE]" "$filename") -ne '0' ];then # 即存在一行以包含 Re 或 RE 的 Subject
+     awk 'NR==1' "$filename" >> new.txt
    fi
 done
-
