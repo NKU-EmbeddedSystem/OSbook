57a9efae2abc3500c7d2fd35fabf636251d6baab
cltinga <91513458+cltinga@users.noreply.github.com>
2023-07-12 15:19:10 +0800

    Update lab1.sh

--- lab_basic/lab1/lab1.sh
+++ lab_basic/lab1/lab1.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-for i in `ls`
+dir=$(dirname "$0")/input
+
+for i in "$dir"/*
 do
     #提取文件名
-    filename=$i
+    filename=$(basename "$i")
     #获取文件的修改时间
-    mtime=`stat -c %Y $filename`
+    mtime=$(stat -c %Y "$i")
     #格式化时间
-    formate_date=`date '+%Y%m%d' -d @$mtime`
+    formate_date=$(date '+%Y%m%d' -d @"$mtime")
     #提取文件的后缀名
     name=${filename#*.}
     #进行拼接
-    mv $i ${formate_date}.${name}
+    mv "$i" "$dir/${formate_date}.${name}"
 done
 
 
