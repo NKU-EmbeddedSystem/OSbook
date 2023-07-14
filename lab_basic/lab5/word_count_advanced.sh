d3dfb860a18b7b66b63b29e563010d245487ef99
HongShao0000 <52363750+HongShao0000@users.noreply.github.com>
2023-07-14 23:51:44 +0800

    Update word_count_advanced.sh

--- lab_basic/lab5/word_count_advanced.sh
+++ lab_basic/lab5/word_count_advanced.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 word="$3"
-files=$(ls *.txt | head -"$2" | tail -$(($2 - $1 + 1)))
+files=$(ls input/*.txt | head -"$2" | tail -$(($2 - $1 + 1)))
 total=0
 for file in $files; do
     count=$(grep -o -i -w "$word" "$file" | wc -l)
