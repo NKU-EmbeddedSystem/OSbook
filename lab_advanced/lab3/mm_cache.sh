745264d8598928fb882c0c553c1c4e8efbcdac49
HongShao0000 <52363750+HongShao0000@users.noreply.github.com>
2023-07-17 02:13:23 +0800

    Update mm_cache.sh

--- lab_advanced/lab3/mm_cache.sh
+++ lab_advanced/lab3/mm_cache.sh
@@ -4,6 +4,6 @@ g++ -o mm_naive mm_naive.cpp
 g++ -o mm_blocked mm_blocked.cpp
 g++ -o mm_opt mm_opt.cpp
 
-perf stat -e cache-references,cache-misses ./mm_naive > output/naive_result.txt
-perf stat -e cache-references,cache-misses ./mm_blocked > output/blocked_result.txt
-perf stat -e cache-references,cache-misses ./mm_opt > output/opt_result.txt
+./mm_naive
+./mm_blocked
+./mm_opt
