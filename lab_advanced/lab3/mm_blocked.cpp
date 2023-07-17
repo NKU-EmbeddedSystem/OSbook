d981468cb3ac7282d1a57c009d7c16c66d69d5d8
HongShao0000 <52363750+HongShao0000@users.noreply.github.com>
2023-07-18 00:56:43 +0800

    Update mm_blocked.cpp

--- lab_advanced/lab3/mm_blocked.cpp
+++ lab_advanced/lab3/mm_blocked.cpp
@@ -9,25 +9,6 @@ using namespace std;
 using namespace std::chrono; // 高精度时间库命名空间
 #define blockSize 128
 
-// 直接实现的矩阵乘法
-vector<vector<float>> matrixMultiplicationDirect(const vector<vector<float>>& A, const vector<vector<float>>& B) {
-    int m = A.size();
-    int n = A[0].size();
-    int p = B[0].size();
-
-    vector<vector<float>> C(m, vector<float>(p, 0.0));
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < p; j++) {
-            for (int k = 0; k < n; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    return C;
-}
-
 // 分块矩阵乘法
 vector<vector<float>> matrixMultiplicationBlocked(const vector<vector<float>>& A, const vector<vector<float>>& B) {
     int m = A.size();
@@ -50,26 +31,7 @@ vector<vector<float>> matrixMultiplicationBlocked(const vector<vector<float>>& A
     return C;
 }
 
-
-//结果验证函数
-bool verifyMatrixEquality(const vector<vector<float>>& A, const vector<vector<float>>& B) {
-    if (A.size() != B.size() || A[0].size() != B[0].size()) {
-        return false;
-    }
-
-    int m = A.size();
-    int n = A[0].size();
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (A[i][j] != B[i][j]) {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
+//写入矩阵结果
 void writeResultToFile(const vector<vector<float>>& result, const string& filename) {
     ofstream outputFile(filename);
 
@@ -88,6 +50,7 @@ void writeResultToFile(const vector<vector<float>>& result, const string& filena
     }
 }
 
+//写入运行时间
 void writeDurationToFile(long long duration, const string& filename) {
     ofstream outputFile(filename);
 
@@ -112,46 +75,37 @@ void performanceTest(const vector<vector<float>>& A, const vector<vector<float>>
     auto duration = duration_cast<nanoseconds>(end - start); // 计算执行时间
     //输出结果
     writeResultToFile(result, "output/result/result_blocked.txt");
-    // for (const auto& row : result) {
-    //     for (const auto& element : row) {
-    //         cout << element << " ";
-    //     }
-    //     cout << endl;
-    // }
+   
     double time = duration.count() / 1e9; // 转换为秒
     double size = A.size(); 
     double gflops = (2.0 * size * size * size) / time / 1e9; // 计算GFLOPS
     writeDurationToFile(duration.count(), "output/time/time_blocked.txt");
     cout << testName << "Time: " << duration.count() << " nanoseconds" << endl;
     cout << testName << " GFLOPS: " << gflops << endl;
-
-    // vector<vector<float>> referenceResult = matrixMultiplicationDirect(A, B);
-    // if (verifyMatrixEquality(referenceResult, result)) {
-    //     cout << "Result correct!" << endl;
-    // } else {
-    //     cout << "Result wrong!" << endl;
-    // }
 }
 
 
 int main() {
-    // 创建两个大矩阵
-    int matrixsize = 2048; // 矩阵大小，可以根据需要调整
-    // ifstream inputFile("input/size.txt");
+    int matrixsize = 2048; // 矩阵大小
+     // 初始化矩阵A
+    vector<vector<float>> A(matrixsize, vector<float>(matrixsize));
+    float valueA = 1.0;
+    for (int i = 0; i < matrixsize; i++) {
+        for (int j = 0; j < matrixsize; j++) {
+            A[i][j] = valueA;
+            valueA += 1.0;
+        }
+    }
 
-    // if (!inputFile.is_open()) {
-    //     cout << "Failed to open input or output file...\n";
-    //     return 0;
-    // }
-
-    // string line;
-    // getline(inputFile, line);
-    // int blocksize, matrixsize;
-    // stringstream ss(line);
-    // ss >> blocksize >> matrixsize;
-
-    vector<vector<float>> A(matrixsize, vector<float>(matrixsize, 1.0));
-    vector<vector<float>> B(matrixsize, vector<float>(matrixsize, 2.0));
+    // 初始化矩阵B
+    vector<vector<float>> B(matrixsize, vector<float>(matrixsize));
+    float valueB = matrixsize * matrixsize + 1.0;
+    for (int i = 0; i < matrixsize; i++) {
+        for (int j = 0; j < matrixsize; j++) {
+            B[i][j] = valueB;
+            valueB *= -1.0;
+        }
+    }
 
     performanceTest(A, B, "Blocked Multiplication: ", matrixMultiplicationBlocked);
     
