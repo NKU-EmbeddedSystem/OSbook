ecb044c3c2a04709d4f7e475f4be89f8250e0777
HongShao0000 <52363750+HongShao0000@users.noreply.github.com>
2023-07-18 00:53:56 +0800

    Update mm_naive.cpp

--- lab_advanced/lab3/mm_naive.cpp
+++ lab_advanced/lab3/mm_naive.cpp
@@ -28,52 +28,6 @@ vector<vector<float>> matrixMultiplicationDirect(const vector<vector<float>>& A,
     return C;
 }
 
-// 分块矩阵乘法
-vector<vector<float>> matrixMultiplicationBlocked1(const vector<vector<float>>& A, const vector<vector<float>>& B) {
-    int m = A.size();
-    int n = A[0].size();
-    int p = B[0].size();
-    vector<vector<float>> C(m, vector<float>(p, 0.0));
-
-    for (int kk = 0; kk < n; kk += blockSize) {
-        for (int jj = 0; jj < p; jj += blockSize) {
-            for (int i = 0; i < m; i++) {
-                for (int j = jj; j < min(jj + blockSize, p); j++) {
-                    for (int k = kk; k < min(kk + blockSize, n); k++) {
-                        C[i][j] += A[i][k] * B[k][j];
-                    }
-                }
-            }
-        }
-    }
-
-    return C;
-}
-
-// cache优化的分块矩阵乘
-vector<vector<float>> matrixMultiplicationBlocked2(const vector<vector<float>>& A, const vector<vector<float>>& B) {
-    int m = A.size();
-    int n = A[0].size();
-    int p = B[0].size();
-    vector<vector<float>> C(m, vector<float>(p, 0.0));
-
-    for (int kk = 0; kk < n; kk += blockSize) {
-        for (int jj = 0; jj < p; jj += blockSize) {
-            for (int i = 0; i < m; i++) {
-                for (int k = kk; k < min(kk + blockSize, n); k++) {
-                    for (int j = jj; j < min(jj + blockSize, p); j++) {
-                        C[i][j] += A[i][k] * B[k][j];
-                    }
-                }
-            }
-        }
-    }
-
-    return C;
-}
-
-
-
 //结果验证函数
 bool verifyMatrixEquality(const vector<vector<float>>& A, const vector<vector<float>>& B) {
     if (A.size() != B.size() || A[0].size() != B[0].size()) {
@@ -135,12 +89,7 @@ void performanceTest(const vector<vector<float>>& A, const vector<vector<float>>
     auto duration = duration_cast<nanoseconds>(end - start); // 计算时间
     //输出结果
     writeResultToFile(result, "output/result/result_naive.txt");
-    // for (const auto& row : result) {
-    //     for (const auto& element : row) {
-    //         cout << element << " ";
-    //     }
-    //     cout << endl;
-    // }
+    
     double time = duration.count() / 1e9; 
     double size = A.size(); 
     double gflops = (2.0 * size * size * size) / time / 1e9; // 计算GFLOPS
@@ -160,22 +109,26 @@ void performanceTest(const vector<vector<float>>& A, const vector<vector<float>>
 int main() {
 
     int matrixsize = 2048; 
-    // ifstream inputFile("input/size.txt");
 
-    // if (!inputFile.is_open()) {
-    //     cout << "Failed to open input or output file...\n";
-    //     return 0;
-    // }
+     // 初始化矩阵A
+    vector<vector<float>> A(matrixsize, vector<float>(matrixsize));
+    float valueA = 1.0;
+    for (int i = 0; i < matrixsize; i++) {
+        for (int j = 0; j < matrixsize; j++) {
+            A[i][j] = valueA;
+            valueA += 1.0;
+        }
+    }
 
-    // string line;
-    // getline(inputFile, line);
-    // int blocksize, matrixsize;
-    // stringstream ss(line);
-    // ss >> blocksize >> matrixsize;
-
-    vector<vector<float>> A(matrixsize, vector<float>(matrixsize, 1.0));
-    vector<vector<float>> B(matrixsize, vector<float>(matrixsize, 2.0));
-
+    // 初始化矩阵B
+    vector<vector<float>> B(matrixsize, vector<float>(matrixsize));
+    float valueB = matrixsize * matrixsize + 1.0;
+    for (int i = 0; i < matrixsize; i++) {
+        for (int j = 0; j < matrixsize; j++) {
+            B[i][j] = valueB;
+            valueB *= -1.0;
+        }
+    }
 
     performanceTest(A, B, "Blocked Multiplication: ", matrixMultiplicationDirect);
     
