cb047fba483a487806335384a6e3924515d60690
learish <1051619126@qq.com>
2023-07-17 23:32:19 +0800

    update check.sh

--- lab_basic/lab3/check.sh
+++ lab_basic/lab3/check.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-# 设置要读取的目录
-directory="output"
+# 计算目录下文件的MD5值
+calculate_md5() {
+    local directory=$1
 
-# 遍历目录中的每个文件
-for file in "$directory"/*; do
-    # 检查文件是否是常规文件
-    if [[ -f "$file" ]]; then
-        # 计算文件内容的哈希值
-        hash=$(md5sum "$file" | awk '{print $1}')
+    # 遍历目录中的所有文件
+    for file in "$directory"/*; do
+        # 忽略子目录
+        if [[ -d $file ]]; then
+            continue
+        fi
 
-        # 输出文件名和内容的哈希值
-        echo "文件: $file"
-        echo "内容哈希值: $hash"
-        echo
-    fi
-done
+        # 计算文件的MD5值
+        local md5=$(md5sum "$file" | awk '{print $1}')
+    done
 
+    echo "$md5"
+}
+
+# 目录1和目录2的路径
+dir1="output/"
+dir2="output/"
+
+# 计算目录1和目录2下文件的MD5值
+md5_1=$(calculate_md5 "$dir1")
+md5_2=$(calculate_md5 "$dir2")
+
+# 比较两个MD5值列表是否相等
+if [[ "$md5_1" == "$md5_2" ]]; then
+    echo "目录1和目录2下的文件MD5值相等"
+    
+else
+    echo "目录1和目录2下的文件MD5值不相等"
+fi
