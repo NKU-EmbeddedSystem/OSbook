78f4fa57730db37ff1fc21bcc3c490aea4aa7970
a1liz <nku_liz@icloud.com>
2023-07-17 23:51:12 +0800

    add comment

--- lab_basic/lab2/lab2.sh
+++ lab_basic/lab2/lab2.sh
@@ -8,7 +8,9 @@ for i in "$dir/ham"/*
 do
    filename=$i
    # 查找主题为回复的邮件并打印第一行内容，即发送者和日期
-   if [ $(grep -c "^Subject: *R[eE]" "$filename") -ne '0' ];then # 即存在一行以包含 Re 或 RE 的 Subject
+   # 如果第一行的Subject 包含 Re 或 RE
+   if [ $(grep -c "^Subject: *R[eE]" "$filename") -ne '0' ];then
+   # 打印第一行内容
      awk 'NR==1' "$filename" >> ./output/new.txt
    fi
 done
