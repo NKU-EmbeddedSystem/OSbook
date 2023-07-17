8f40dbae314220dbaed117633273bda3dd3be6b3
kjr829 <77659571+kjr829@users.noreply.github.com>
2023-07-17 17:28:55 +0800

    Update lab3.c

--- lab_basic/lab3/lab3.c
+++ lab_basic/lab3/lab3.c
@@ -9,65 +9,104 @@
 #include<sys/types.h>
 #include<regex.h>
 
-int main(){
-    int fd=open("input/new.txt",O_RDONLY);
-    if(fd==-1){
-        printf("can't open the file");
+int readSystemCall(int fd, regex_t reg, char* output) {
+    int len = 1;
+    int status = 0;
+    regmatch_t pmatch[1];
+    int count = 0;
+
+    while (len > 0) {
+        char buf[2048] = {"\0"};
+        len = read(fd, buf, 2048);
+        char* temp = buf;
+        while (1) {
+            status = regexec(&reg, temp, 1, pmatch, 0);
+            if (status == 0) {
+                count++;
+                strncpy(output, temp + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
+                printf("matched: %s\n", output);
+                temp += pmatch[0].rm_eo;
+            } else {
+                break;
+            }
+        }
+    }
+
+    return count;
+}
+
+int mmapSystemCall(int fd, regex_t reg, char* output) {
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) {
+        printf("fstat error!\n");
+        return -1;
+    }
+
+    char* mmapped;
+    if ((mmapped = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0)) == (void*)-1) {
+        printf("mmapped error!\n");
+        return -1;
+    }
+
+    close(fd);
+
+    int status = 0;
+    regmatch_t pmatch[1];
+    int count = 0;
+
+    while (1) {
+        status = regexec(&reg, mmapped, 1, pmatch, 0);
+        if (status == 0) {
+            count++;
+            strncpy(output, mmapped + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
+            printf("matched: %s\n", output);
+            mmapped += pmatch[0].rm_eo;
+        } else {
+            break;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    int fd = open("input/new.txt", O_RDONLY);
+    if (fd == -1) {
+        printf("can't open the file\n");
         return 1;
     }
-    int len=1;
-    int status=0;
+
+    int len = 1;
+    int status = 0;
     regmatch_t pmatch[1];
     regex_t reg;
-    int count=0;
-    char pattern[]="^From ilug-admin@linux.ie.*Aug.*";//查找ilug-admin@linux.ie在八月份发送的邮件
-     
-    status=regcomp(&reg,pattern,REG_EXTENDED|REG_NEWLINE);
-    if(status!=0){
+    int count = 0;
+    char pattern[] = "^From ilug-admin@linux.ie.*Aug.*"; // 查找ilug-admin@linux.ie在八月份发送的邮件
+
+    status = regcomp(&reg, pattern, REG_EXTENDED | REG_NEWLINE);
+    if (status != 0) {
         printf("compile error!\n");
         return -1;
     }
-    char output[1024]={"\0"};
 
-    //read 系统调用
-    /*while(len>0){
-        char buf[2048]={"\0"};
-        len=read(fd,buf,2048);
-        char *temp=buf;
-        while(1){
-            status=regexec(&reg,temp,1,pmatch,0);
-            if(status==0){
-                count++;
-                strncpy(output,temp+pmatch[0].rm_so,pmatch[0].rm_eo-pmatch[0].rm_so);
-                printf("matched:%s\n",output);
-                temp += pmatch[0].rm_eo;
-            }
-            else break;
-        }
-    }*/
+    char output[1024] = {"\0"};
 
+    // Using read system call
+    count = readSystemCall(fd, reg, output);
+    printf("The number of matched strings (read): %d\n", count);
 
-
-    //mmap 系统调用
-    struct stat sb;
-    if(fstat(fd,&sb)==-1) printf("fstat error!");
-    char *mmapped;
-    if((mmapped=mmap(NULL,sb.st_size,PROT_READ,MAP_SHARED,fd,0))==(void *)-1) printf("mmapped error!");
+    // Reopening the file for mmap system call
     close(fd);
-
-    while(1){
-        status=regexec(&reg,mmapped,1,pmatch,0);
-        if(status==0){
-            count++;
-            strncpy(output,mmapped+pmatch[0].rm_so,pmatch[0].rm_eo-pmatch[0].rm_so);
-            printf("matched:%s\n",output);
-            mmapped += pmatch[0].rm_eo;
-        }
-        else break;
+    fd = open("input/new.txt", O_RDONLY);
+    if (fd == -1) {
+        printf("can't open the file\n");
+        return 1;
     }
 
+    // Using mmap system call
+    count = mmapSystemCall(fd, reg, output);
+    printf("The number of matched strings (mmap): %d\n", count);
 
     regfree(&reg);
-    printf("the number of matched string:%d\n",count);
     return 0;
 }
