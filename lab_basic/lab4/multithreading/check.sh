08b214a46c5efa90380acc973e29e8ad6b5defd8
learish <1051619126@qq.com>
2023-07-17 23:58:39 +0800

    update check.sh

--- lab_basic/lab4/multithreading/check.sh
+++ lab_basic/lab4/multithreading/check.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-# 设置要读取的目录
-directory="output"
+# 读取文件内容并排序
+sort_file() {
+    local file=$1
 
-# 遍历目录中的每个文件
-for file in "$directory"/*; do
-    # 检查文件是否是常规文件
-    if [[ -f "$file" ]]; then
-        # 计算文件内容的哈希值
-        hash=$(md5sum "$file" | awk '{print $1}')
+    # 除倒数两行外，读取文件内容并排序
+    sorted_content=$(head -n -2 "$file" | sort)
 
-        # 输出文件名和内容的哈希值
-        echo "文件: $file"
-        echo "内容哈希值: $hash"
-        echo
-    fi
-done
+    # 输出排序后的内容
+    echo "$sorted_content"
+}
 
+# 文件1和文件2的路径
+file1="output/lab4_process_result.txt"
+file2="output/lab4_process_result.txt"
+
+# 读取并排序文件1的内容
+sorted_content1=$(sort_file "$file1")
+
+# 读取并排序文件2的内容
+sorted_content2=$(sort_file "$file2")
+
+# 比较两个排序结果的MD5值
+md5_1=$(echo "$sorted_content1" | md5sum | awk '{print $1}')
+md5_2=$(echo "$sorted_content2" | md5sum | awk '{print $1}')
+
+# 输出比较结果
+echo "文件1的排序结果的MD5值: $md5_1"
+echo "文件2的排序结果的MD5值: $md5_2"
+
+# 比较MD5值是否相等
+if [[ "$md5_1" == "$md5_2" ]]; then
+    echo "文件1和文件2的排序结果的MD5值相等"
+else
+    echo "文件1和文件2的排序结果的MD5值不相等"
+fi
