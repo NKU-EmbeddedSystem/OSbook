ed0262d427b3ce7fb4a366cb55ee3098a6c15b88
HongShao0000 <52363750+HongShao0000@users.noreply.github.com>
2023-07-18 23:41:20 +0800

    Update check.sh

--- lab_advanced/lab3/check.sh
+++ lab_advanced/lab3/check.sh
@@ -19,14 +19,14 @@ md5_output_blocked=$(md5sum "output/result/result_blocked.txt" | awk '{print $1}
 md5_output_opt=$(md5sum "output/result/result_opt.txt" | awk '{print $1}')
 
 if [ "$md5_output_blocked" == "$md5_output_naive" ]; then
-    echo "输出文件blocked MD5值匹配，lab3-1最终分数为: 100"
+    echo "输出文件blocked MD5值匹配，lab3-1最终分数为: 50"
 else
     echo "输出文件blocked MD5值不匹配，lab3-1最终分数为: 0"
     echo $md5_output_blocked
     echo $md5_output_naive
 fi
 if [[ "$md5_output_opt" == "$md5_output_naive" ]]; then
-    echo "输出文件MD5值匹配，lab3-2最终分数为: 100"
+    echo "输出文件MD5值匹配，lab3-2最终分数为: 50"
 else
     echo "输出文件MD5值不匹配，lab3-2最终分数为: 0"
     echo $md5_output_opt
