8d775acfca155a95a6538d3aa6bbde1562c667a4
a1liz <nku_liz@icloud.com>
2023-07-18 23:52:26 +0800

    update check.sh

--- lab_advanced/lab3/check.sh
+++ lab_advanced/lab3/check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ ! -e "output/result/result_naive.txt" ]; then
-    echo "基础矩阵乘法输出文件不存在，lab3-1最终分数为: 0。"
+    echo "基础矩阵乘法输出文件不存在"
     exit 1
 fi
 if [ ! -e "output/result/result_blocked.txt" ]; then
-    echo "分块矩阵乘法输出文件不存在，lab3-2最终分数为: 0。"
+    echo "分块矩阵乘法输出文件不存在"
     exit 1
 fi
 if [ ! -e "output/result/result_opt.txt" ]; then
-    echo "优化矩阵乘法输出文件不存在，lab3-3最终分数为: 0。"
+    echo "优化矩阵乘法输出文件不存在"
     exit 1
 fi
 
@@ -17,28 +17,22 @@ fi
 md5_output_naive=$(md5sum "output/result/result_naive.txt" | awk '{print $1}')
 md5_output_blocked=$(md5sum "output/result/result_blocked.txt" | awk '{print $1}')
 md5_output_opt=$(md5sum "output/result/result_opt.txt" | awk '{print $1}')
-md5_target=$(md5sum "output/result/result_reference.txt" | awk '{print $1}')
 
-# 比较两个MD5值列表是否相等
-if [[ "$md5_output_naive" == "$md5_target" ]]; then
-    echo "输出文件naive MD5值匹配，lab3-1分数为: 100。"
+if [ "$md5_output_blocked" == "$md5_output_naive" ]; then
+    echo "输出文件blocked MD5值匹配，lab3-1最终分数为: 50"
 else
-    echo "输出文件naive MD5值不匹配，lab3-1最终分数为: 0。"
+    echo "输出文件blocked MD5值不匹配，lab3-1最终分数为: 0"
+    echo $md5_output_blocked
     echo $md5_output_naive
 fi
-if [[ "$md5_output_blocked" == "$md5_target" ]]; then
-    echo "输出文件blocked MD5值匹配，lab3-2最终分数为: 100。"
+if [[ "$md5_output_opt" == "$md5_output_naive" ]]; then
+    echo "输出文件MD5值匹配，lab3-2最终分数为: 50"
 else
-    echo "输出文件blocked MD5值不匹配，lab3-2最终分数为: 0。"
-    echo $md5_output_blocked
-fi
-if [[ "$md5_output_opt" == "$md5_target" ]]; then
-    echo "输出文件MD5值匹配，lab3-3最终分数为: 100。"
-else
-    echo "输出文件MD5值不匹配，lab3-3最终分数为: 0。"
+    echo "输出文件MD5值不匹配，lab3-2最终分数为: 0"
     echo $md5_output_opt
+    echo $md5_output_naive
 fi
 
-if [ "$md5_output_naive" == "$md5_target" ] && [ "$md5_output_blocked" == "$md5_target" ] && [ "$md5_output_opt" == "$md5_target" ]; then
-    echo "lab3-3最终分数为: 100。"
+if  [ "$md5_output_blocked" == "$md5_output_naive" ] && [ "$md5_output_opt" == "$md5_output_naive" ]; then
+    echo "lab3最终分数为: 100"
 fi
